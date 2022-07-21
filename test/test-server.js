0a85c0e80d6b48eed1218815ac0677bbc74d9ae6
Chris Plummer <chris.plummer@doma.com>
2022-07-21 08:10:08 -0500

    fix: switch to cross-fetch from request (#7)

---
+++ test/test-server.js
@@ -0,0 +1,11 @@
+if (require.main === module) {
+  const express = require("express");
+  const app = express();
+
+  const jsonFixture = require("./fixtures/json/valid.json");
+  app.get("/valid", function (req, res) {
+    res.status(200).json(jsonFixture);
+  });
+
+  server = app.listen(9002, "localhost");
+}
